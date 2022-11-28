ab87508fa3f9668a29332eb15b3e939d0f03886d
cuberhaus <polcg10@gmail.com>
2022-11-28 22:42:26 +0100

    This is a placeholder

--- lab3_code/run-lite-tareador.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-filename=$1
-shift
-options=$@
-echo "filename:$filename"
-echo "options:$options"
-source /Soft/PAR/environment.bash
-rm -rf .tareador_precomputed_$filename
-tareador_gui.py --llvm --lite $filename -b "-w 8 $options"
